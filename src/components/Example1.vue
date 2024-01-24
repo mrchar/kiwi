a3d86300630430992deda18e24f86940815f8d14
mrchar <mrchar@qq.com>
2024-01-25 00:40:08 +0800

    添加示例

---
+++ src/components/Example1.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { en, zh, locale, setLocale, m } from "../composables/i18n"
+
+const message = m({ zh: "你好！", en: "Hello!" })
+
+const language = computed(() => {
+  switch (locale.value) {
+    case zh:
+      return "中文"
+    case en:
+      return "english"
+  }
+})
+
+function taggleLanguage() {
+  locale.value === zh ? setLocale(en) : setLocale(zh)
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <button @click="taggleLanguage">{{ language }}</button>
+</template>
+
+<style scoped>
+div {
+  margin: 1rem;
+  font-size: 2rem;
+}
+</style>
---
+++ src/components/Example2.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { en, zh, locale, setLocale, t } from "../composables/i18n"
+
+const language = computed(() => {
+  switch (locale.value) {
+    case zh:
+      return "中文"
+    case en:
+      return "english"
+  }
+})
+
+function taggleLanguage() {
+  locale.value === zh ? setLocale(en) : setLocale(zh)
+}
+</script>
+
+<template>
+  <div>{{ t({ zh: "你好！", en: "Hello!" }) }}</div>
+  <button @click="taggleLanguage">{{ language }}</button>
+</template>
+
+<style scoped>
+div {
+  margin: 1rem;
+  font-size: 2rem;
+}
+</style>
